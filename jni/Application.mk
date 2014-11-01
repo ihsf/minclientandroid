ee49944b0c295732db324d1b2421f0d370bd0162
Bartosz Taudul <wolf.pld@gmail.com>
2014-11-01 19:07:42 +0100

    Also compile for armv7.

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,7 +2,7 @@
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
  APP_STL := gnustl_static
- APP_ABI := armeabi x86
+ APP_ABI := armeabi x86 armeabi-v7a
  APP_PLATFORM := android-15
  NDK_TOOLCHAIN_VERSION=4.8
 
