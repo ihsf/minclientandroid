6154c938a675c8c4708c1ed07b963bd00dabfe0b
Bartosz Taudul <wolf.pld@gmail.com>
2014-11-01 19:06:31 +0100

    Use gnustl.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,7 @@
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
- APP_STL := stlport_static
+ APP_STL := gnustl_static
  APP_ABI := armeabi x86
  APP_PLATFORM := android-15
  NDK_TOOLCHAIN_VERSION=4.8
