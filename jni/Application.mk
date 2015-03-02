e21a1c1ac905eb861dee90f73488479e44d68bef
elchtest <winclan@freenet.de>
2015-03-02 16:32:45 +0100

    ====== beyond journal paper. fixed target to android-19.

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,6 +3,6 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
  APP_STL := gnustl_static
  APP_ABI := armeabi x86 armeabi-v7a
- APP_PLATFORM := android-15
+ APP_PLATFORM := android-19
  NDK_TOOLCHAIN_VERSION=4.8
 
