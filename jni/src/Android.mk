fb6c9c7a9d2f5034296c8be1188169ad0388e54f
elchtest <winclan@freenet.de>
2015-03-10 15:44:51 +0100

    Cleanups and license.txt

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -6,12 +6,9 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 SDL_NET_PATH := ../SDL_net
-#FFMPEG_PATH := ../ffmpeg/ffmpeg/armeabi
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
   $(LOCAL_PATH)/$(SDL_NET_PATH) 
-#  \
-#  $(LOCAL_PATH)/$(FFMPEG_PATH)/include 
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
@@ -35,12 +32,8 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	../../../minclient/Sensors.cpp \
 	../lz4-r123/lz4.c
 
-# LOCAL_SHARED_LIBRARIES := SDL2 SDL2_net 
-#avcodec avutil avformat avfilter avdevice swscale
- LOCAL_SHARED_LIBRARIES := SDL2 SDL2_net 
- #avcodec avutil avformat
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_net 
 
-# LOCAL_LDLIBS := -lGLESv1_CM -llog 
 LOCAL_LDLIBS := -lGLESv2 -llog -lEGL
 
 LOCAL_ARM_MODE := arm
